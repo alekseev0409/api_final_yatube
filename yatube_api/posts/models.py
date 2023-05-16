a0473e7f0c145f43536ac69d31d0497512fc2090
Алексей Алексеев <alekseev0409@gmail.com>
2023-05-16 22:56:21 +0300

    vse

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -51,24 +51,6 @@ class Follow(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='follower',
-        verbose_name='Подписчик'
-    )
-    following = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='following',
-        verbose_name='Автор'
-    )
-
-    def __str__(self):
-        return f'{self.user.username}, {self.following.username}'
-
-
-class Follow(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='follower',
     )
     following = models.ForeignKey(
         User,
